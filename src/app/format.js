9e253c672c39bb0d85d6a17f33b88f750e939dfb
drew <andrew.wr.evans@gmail.com>
2021-09-09 19:13:39 +0200

    updated date timestamp logic

--- src/app/format.js
+++ src/app/format.js
@@ -1,17 +1,17 @@
 export const formatDate = (dateStr) => {
-  const date = new Date(dateStr);
-  let newDate = "";
-  const ye = new Intl.DateTimeFormat("fr", { year: "numeric" }).format(newDate);
-  const mo = new Intl.DateTimeFormat("fr", { month: "short" }).format(newDate);
-  const da = new Intl.DateTimeFormat("fr", { day: "2-digit" }).format(newDate);
-  const month = mo.charAt(0).toUpperCase() + mo.slice(1);
+  if (!isNaN(dateStr)) {
+    return null;
+  } else {
+    const date = new Date(dateStr);
+    const ye = new Intl.DateTimeFormat("fr", { year: "numeric" }).format(date);
+    const mo = new Intl.DateTimeFormat("fr", { month: "short" }).format(date);
+    const da = new Intl.DateTimeFormat("fr", { day: "2-digit" }).format(date);
+    const month = mo.charAt(0).toUpperCase() + mo.slice(1);
 
-  if (!isNaN(date)) {
-    newDate = Date.parse(date);
+    return `${parseInt(da)} ${month.substr(0, 3)}. ${ye
+      .toString()
+      .substr(2, 4)}`;
   }
-  newDate = new Date(date);
-
-  return `${parseInt(da)} ${month.substr(0, 3)}. ${ye.toString().substr(2, 4)}`;
 };
 
 export const formatStatus = (status) => {
