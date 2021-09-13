d4b6462b3d5fadba700fd2d568dc2570b371c4a2
Andrew E <45772398+DrewEvans@users.noreply.github.com>
2021-09-13 13:47:18 +0200

    additional test added - bills

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,25 +1,64 @@
 import { screen } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
+import Bill from "../containers/Bills.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import { ROUTES } from "../constants/routes.js";
+import userEvent from "@testing-library/user-event";
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: [] });
-      document.body.innerHTML = html;
-      //to-do write expect expression
-    });
-    test("Then bills should be ordered from earliest to latest", () => {
-      const html = BillsUI({ data: bills });
-      document.body.innerHTML = html;
-      const dates = screen
-        .getAllByText(
-          /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
-        )
-        .map((a) => a.innerHTML);
-      const antiChrono = (a, b) => a - b;
-      const datesSorted = [...dates].sort(antiChrono);
-      expect(dates).toEqual(datesSorted);
-    });
-  });
+	describe("When I am on Bills Page", () => {
+		test("Then bill icon in vertical layout should be highlighted", () => {
+			const html = BillsUI({ data: [] });
+			document.body.innerHTML = html;
+			//to-do write expect expression
+		});
+		test("Then bills should be ordered from earliest to latest", () => {
+			const html = BillsUI({ data: bills });
+			document.body.innerHTML = html;
+			const dates = screen
+				.getAllByText(
+					/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
+				)
+				.map((a) => a.innerHTML);
+			const antiChrono = (a, b) => a - b;
+			const datesSorted = [...dates].sort(antiChrono);
+			expect(dates).toEqual(datesSorted);
+		});
+	});
+	describe("When user clicks the eye icon", () => {
+		test("A modal appears", () => {
+			Object.defineProperty(window, "localStorage", {
+				value: localStorageMock,
+			});
+			window.localStorage.setItem(
+				"user",
+				JSON.stringify({
+					type: "Employee",
+				})
+			);
+			const html = BillsUI(bills[0]);
+			document.body.innerHTML = html;
+			const onNavigate = (pathname) => {
+				document.body.innerHTML = ROUTES({ pathname });
+			};
+
+			const firestore = null;
+			const bill = new Bill({
+				document,
+				onNavigate,
+				firestore,
+				localStorage: window.localStorage,
+			});
+
+			const handleClickIconEye = jest.fn(bill.handleClickIconEye);
+			const eye = screen.getByTestId("icon-eye");
+			eye.addEventListener("click", handleClickIconEye);
+			userEvent.click(eye);
+			expect(handleClickIconEye).toHaveBeenCalled();
+
+			const modal = screen.getByTestId("modaleFile");
+			expect(modal).toBeTruthy();
+		});
+	});
 });
