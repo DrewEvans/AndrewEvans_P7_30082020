c6e39cd5d4355394bf2ea02bce3e67b01ca5c4ed
drew <andrew.wr.evans@gmail.com>
2021-09-17 20:57:07 +0200

    updates to new bill test

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,12 +1,9 @@
-import { screen } from "@testing-library/dom";
+import { fireEvent, screen } from "@testing-library/dom";
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
-import e from "express";
-import { bills } from "../fixtures/bills.js";
 import userEvent from "@testing-library/user-event";
 import { ROUTES } from "../constants/routes.js";
-import firebase from "../__mocks__/firebase.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
@@ -20,8 +17,29 @@ describe("Given I am connected as an employee", () => {
       document.body.innerHTML = NewBillUI();
       const form = screen.getByTestId("form-new-bill");
       expect(form).toBeTruthy();
-      expect(form.length).toEqual(9);
     });
+
+    describe("and a file is uploaded", () =>{
+      test("The file should be registered", ()=>{
+        document.body.innerHTML = NewBillUI();
+        
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          firestore: firestore,
+          localStorage: window.localStorage,
+        })
+
+        const handleChange = jest.fn((e) => newBill.handleChangeFile(e));
+        const file = screen.getByTestId("file");
+
+        file.addEventListener("change", handleChange)
+        userEvent.upload(file, target)
+        
+        expect(handleChange).toBeCalled();
+        expect(file).toBeTruthy();
+      })
+    })
 
     describe("When I submit the form", () => {
       test("An object should be created", () => {
@@ -41,15 +59,36 @@ describe("Given I am connected as an employee", () => {
         const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
         const submitBtn = screen.getByTestId("form-new-bill");
 
-        /**
-         * todo: Validate field values, expected(newBill).toMatch(tempValidBill)  Value on create
-         */
+        const expectedBill = {
+          type: "Transports",
+          name: "bill",
+          amount: 1,
+          date: "2021-01-01",
+          vat: 20,
+          pct: 10,
+          commentary: "", 
+          fileName: "bill.jpg",
+          fileURL: "https://test/test.jpg",
+        };
+       
+        newBill.createBill = (newBill) => newBill;
+        
+        screen.getByTestId("expense-type") = expectedBill.type;
+        screen.getByTestId("expense-name") = expectedBill.name;
+        screen.getByTestId("amount") = expectedBill.amount;
+        screen.getByTestId("datepicker") = expectedBill.date;
+        screen.getByTestId("vat") = expectedBill.vat;
+        screen.getByTestId("pct") = expectedBill.pct;
+        screen.getByTestId("commentary") = expectedBill.commentary;
+        
+        newBill.fileURL = expectedBill.fileURL
+        newBill.fileName = expectedBill.fileName
+
 
         submitBtn.addEventListener("click", handleSubmit);
+        fireEvent.click(submit),
         userEvent.click(submitBtn);
         expect(handleSubmit).toHaveBeenCalled();
-
-        newBill.createBill = (newBill) => newBill;
       });
 
       test("I should be redirected to the bills Page", () => {
